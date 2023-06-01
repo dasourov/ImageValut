0cd08af2d52e6f5c53cc7bbbfcb106772c04c92c
Didarul Alam Sourov <da.sourov.11@gmail.com>
2023-06-01 10:20:59 -0700

    First-version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      TopNav: "#232228",
+      SubNav: "#35353d",
+      Body: " #3c3c44",
+      white: "#ffffff",
+      black: "#000000",
+    },
+  },
+  plugins: [],
+};
